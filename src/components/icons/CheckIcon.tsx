7b57bdc1d32afd4461aecb99d0a8be30f87dd48a
unknown <junior.nascm@gmail.com>
2024-02-02 01:33:09 -0300

    feat: adding more icons

---
+++ src/components/icons/CheckIcon.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from "react"
+
+export default function CheckIcon(props: ComponentProps<'svg'>) {
+  return (
+    <svg width={10} height={8} xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        stroke="#FFF"
+        strokeWidth={2}
+        fill="none"
+        d="M1.276 3.066l2.756 2.756 5-5"
+      />
+    </svg>
+  )
+}
