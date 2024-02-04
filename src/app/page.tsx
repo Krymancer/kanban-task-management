3b3c287e0a11720a9b271c0ba19be36fbd5fa102
unknown <junior.nascm@gmail.com>
2024-02-04 16:51:17 -0300

    feat: header mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import LogoDark from '@/assets/logo-dark.svg';
-import LogoLight from '@/assets/logo-light.svg';
-import LogoMobile from '@/assets/logo-mobile.svg';
-import ChevronDown from '@/assets/icon-chevron-down.svg'
-import ChevronUp from '@/assets/icon-chevron-up.svg'
-import VerticalEllipsis from '@/assets/icon-vertical-ellipsis.svg';
-import HiideSidebarIcon from '@/assets/icon-hide-sidebar.svg';
-import CrossIcon from '@/assets/icon-cross.svg';
-import BoardIcon from '@/assets/icon-board.svg' 
-
 import Button from '@/components/Button';
 import ThemeToggle from '@/components/ThemeToggle';
 
-import Image from 'next/image';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+
+import LogoLight from '@/components/logos/LogoLight';
+import LogoMobile from '@/components/logos/LogoMobile';
+import LogoDark from '@/components/logos/LogoDark';
+import ChevronDownIcon from '@/components/icons/ChevronDownIcon';
+import BoardIcon from '@/components/icons/BoardIcon';
+import VerticalEllipsisIcon from '@/components/icons/VerticalEllipsisIcon';
 
 function SidebarBoardItem({ active} : { active?: boolean }) {
   return (
     <div className='w-full flex justify-start'>
       <div className={`${active ? 'bg-main-purple text-white' : 'text-medium-gray'} rounded-r-[100px] flex items-center gap-3 px-6 py-4 font-bold`} >
-        <Image src={BoardIcon} alt='icon' width={16} height={16} />
+        <BoardIcon />
         Platform Launch
       </div>
     </div>
@@ -29,18 +25,23 @@ function SidebarBoardItem({ active} : { active?: boolean }) {
 
 function Header() {
   return (
-    <header className='flex justify-between px-4 py-5 items-center'>
+    <header className='flex justify-between px-4 py-5 items-center dark:bg-dark-gray'>
       <div className='flex gap-4'>
         <div>
-          <Image src={LogoLight} width={152.53} height={25.22} alt='' className='hidden dark:sm:block' />
-          <Image src={LogoDark} width={152.53} height={25.22} alt='' className='hidden sm:block dark:hidden' />
-          <Image src={LogoMobile} width={24} height={25} alt='' className='block sm:hidden' />
+          <LogoLight className='hidden dark:sm:block' />
+          <LogoDark className='hidden sm:block dark:hidden' />
+          <LogoMobile className='block sm:hidden' />
+          {/* <Image src={LogoLight} width={152.53} height={25.22} alt='' className='' /> */}
+          {/* <Image src={LogoDark} width={152.53} height={25.22} alt='' className='' /> */}
+          {/* <Image src={LogoMobile} width={24} height={25} alt='' className='block sm:hidden' /> */}
         </div>
 
+        <div className='hidden sm:block text-2xl font-bold'>Platform Launch</div>
+
         <Dialog >
-          <DialogTrigger className='text-lg font-bold flex gap-2 items-center'>
+          <DialogTrigger className='text-lg font-bold flex gap-2 items-center dark:text-white sm:hidden'>
               Platform Launch
-              <Image src={ChevronDown} width={8} height={4} alt='' />            
+              <ChevronDownIcon />              
           </DialogTrigger>
           <DialogContent className='w-[264px] rounded-lg p-0 border-none flex flex-col gap-0'>
             <div className='px-6 py-4 font-bold text-medium-gray text-[12px] tracking-[2.4px]'>ALL BOARDS (3)</div>
@@ -61,7 +62,7 @@ function Header() {
         <Button className='min-w-12 w-12' onClick={() => { }}>
           +
         </Button>
-        <Image src={VerticalEllipsis} width={3.692} height={16} alt='' />
+        <VerticalEllipsisIcon />
       </div>
     </header>
   )
@@ -81,7 +82,7 @@ export default function Home() {
   return (
     <main className='h-full min-h-screen flex flex-col'>
       <Header />
-      <div className='bg-light-gray flex flex-col flex-auto items-center justify-center'>
+      <div className='bg-light-gray flex flex-col flex-auto items-center justify-center dark:bg-very-dark-gray'>
         <Columns />
       </div>
     </main>
