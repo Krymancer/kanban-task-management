2b1805d68bbcaa1330f9e4c0fb870987e42a4156
Júnior Nascimento <junior.nascm@gmail.com>
2024-04-25 13:48:16 -0300

    feat: styling checkbox

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -3,35 +3,53 @@
 import CheckboxOld from "@/components/Checkbox";
 import Dropdown from "@/components/Dropdown";
 import TextField from "@/components/TextField";
+import { ThemeToggle } from "@/components/theme-toogle";
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useState } from "react";
 
 export default function ComponentsPage() {
+  const [checked, setChecked] = useState(false);
+  const [value, setValue] = useState('');
+
+  const items = [
+    { label: 'Todo', value: 'Todo' },
+    { label: 'Doing', value: 'Doing' },
+    { label: 'Done', value: 'Done' },
+  ];
+
+
   return (
-    <div className="min-h-screen flex items-center justify-center gap-10 p-40">
-      <div className="flex flex-col space-y-4 w-full">
-        <Dropdown onChange={() => { }} items={[{ label: 'Todo', value: 'Todo' }]} selected={{ label: 'Todo', value: 'Todo' }} />
-        <CheckboxOld label="Checkbox" onChange={() => { }} />
-        <TextField label="Text Field" value="123" onChange={() => { }} />
+    <>
+      <ThemeToggle />
+      <div className="min-h-screen flex items-center justify-center gap-10 p-40">
+        <div className="flex flex-col space-y-4 w-full">
+          <CheckboxOld label="Checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+          <TextField label="Text Field" value={value} onChange={(e) => setValue(e.target.value)} />
+          <Dropdown onChange={() => { }} items={items} selected={items[0]} />
+        </div>
+        <div className="flex flex-col space-y-4 w-full">
+          <div className="flex gap-4 items-center bg-light-gray dark:bg-very-dark-gray h-10 p-3 rounded-md hover:bg-main-purple-hover/25 dark:hover:bg-main-purple-hover/25">
+            <Checkbox id="checkbox" />
+            <label htmlFor="checkbox" className="text-xs font-bold peer-data-[state=checked]:line-through">Checkbox</label>
+          </div>
+          <Input />
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select..." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="todo">Todo</SelectItem>
+                <SelectItem value="doing">Doing</SelectItem>
+                <SelectItem value="done">Done</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-      <div className="flex flex-col space-y-4 w-full">
-        <Checkbox>Teste</Checkbox>
-        <Input />
-        <Select>
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Select..." />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="todo">Todo</SelectItem>
-              <SelectItem value="doing">Doing</SelectItem>
-              <SelectItem value="done">Done</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
-    </div>
+    </>
   );
 }
